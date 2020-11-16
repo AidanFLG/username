e91050efe6bd032e896dc939e445b4846a3087e7
Aidan Lewis-Grenz <lewisgrenz@chapman.edu>
2020-11-15 23:01:44 -0800

    add username program

--- username.sh
+++ username.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 # username.sh
 # Aidan Lewis-Grenz
-echo "Enter a username: "
+echo "Enter a username"
 echo "Use lower case characters, digits and underscore"
-echo " It must start with a lower case character"
-echo " It must contain at least 3 but not more than 12 characters"
+echo "It must start with a lower case character"
+echo "It must contain at least 3 but not more than 12 characters"
 read USERNAME
-while echo "$USERNAME" | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
+while echo "$USERNAME" | egrep -v "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid username code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
-	read ZIP
+	echo "You must enter a valid username - starting with a lower case letter, 3-12 characters, and only using lower case letters, digits, and underscores!"
+	echo "Enter a valid username: "
+	read USERNAME
 done
 echo "Thank you"
